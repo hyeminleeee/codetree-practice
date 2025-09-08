53e2cad5921f2ba54cff5bd7968db242e07872b8
이혜민 <167519616+hyeminleeee@users.noreply.github.com>
2025-09-08 14:34:09 +0900

    Title: 배수의 개수, Time: 137ms, Memory: 10MB, Status: Passed - Codetree

--- 250908/배수의 개수/number-of-multipliers.java
+++ 250908/배수의 개수/number-of-multipliers.java
@@ -5,22 +5,17 @@ public class Main {
         Scanner sc=new Scanner(System.in);
         int a, cnt3=0, cnt5=0;
 
-        //3의 배수 갯수
         for(int i=0; i < 10; i++){
             a=sc.nextInt();
             
-            if(arr[i] % 3 ==0){
+            if(a % 3 ==0){
                 cnt3++;
             }
-            if(arr[i] % 5 == 0){
+            if(a % 5 == 0){
                 cnt5++;
             }
         }
-
-        //5의 배수 갯수
-        for(int i=0; i < 10; i++){
-        }   
+  
         System.out.print(cnt3 + " " + cnt5);
-
     }
 }
